a6dccfc1537bc215ca0045ce70361ae5e04deeb7
Jun Eeo <jun.eeo@thehutgroup.com>
2023-03-27 18:13:56 +0100

    [ICE-101] bump CAPO

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -7,7 +7,7 @@ APPLICATION_NAME="cluster-api-provider-openstack"
 # keep in sync with 
 # * image tag in helm/cluster-api-provider-openstack/values.yaml
 # * used capo-version in mc-bootstrap - defined in Makefile.custom.mk (https://github.com/giantswarm/mc-bootstrap/blob/main/Makefile.custom.mk)
-CAPO_VERSION="v0.6.3"
+CAPO_VERSION="v0.7.1"
 
 .PHONY: all
 all: fetch-upstream-manifest apply-kustomize-patches delete-generated-helm-charts release-manifests ## Builds the manifests to publish with a release (alias to release-manifests)
@@ -30,3 +30,10 @@ apply-kustomize-patches: ## apply giantswarm specific patches
 delete-generated-helm-charts: # clean workspace and delete manifests
 	@rm -rvf ./helm/${APPLICATION_NAME}/templates/*.yaml
 	@rm -rvf ./helm/${APPLICATION_NAME}/crds/*.yaml
+
+ensure-schema-gen:
+	@helm schema-gen --help &>/dev/null || helm plugin install https://github.com/mihaisee/helm-schema-gen.git
+
+.PHONY: schema-gen
+schema-gen: ensure-schema-gen ## Generates the values schema file
+	@cd helm/cluster-api-provider-openstack && helm schema-gen values.yaml > values.schema.json && git diff values.schema.json
